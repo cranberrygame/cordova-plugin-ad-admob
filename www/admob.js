5bdd70ddb1acc95efc4635891c9250324b8debab
Sang Ki Kwon <cranberrygame@yahoo.com>
2016-07-28 03:31:04 +0900

    Update admob.js

--- www/admob.js
+++ www/admob.js
@@ -33,8 +33,8 @@ module.exports = {
 														
 //fixCocoonIOCordovaAndroidAdMobIssue
 if (typeof Cocoon != 'undefined' && navigator.userAgent.match(/Android/i) && !self._fixCocoonIOCordovaAndroidAdMobIssue) {
-	self.reloadBannerAd();
-//	self._fixCocoonIOCordovaAndroidAdMobIssue=true;
+//	self.reloadBannerAd();
+	self._fixCocoonIOCordovaAndroidAdMobIssue=true;
 }							
 					}
 					else if (result == "onBannerAdShown") {
